da849b0a20ace662ffb8de67e932b82eaf42d374
Sumanth-Narasimhulu <137056321+Sumanth-Narasimhulu@users.noreply.github.com>
2026-06-30 10:16:17 +0530

    2 ms (98.41%) 45.1 MB (56.82%) 06-30-2026 1897-redistribute-characters-to-make-all-strings-equal Easy .java

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
@@ -1,16 +1,20 @@
 class Solution {
     public boolean makeEqual(String[] words) {
-        HashMap<Character,Integer>map = new HashMap<>();
-        if(words.length==1)return true;
-        for(String s:words){
-            for(int i=0;i<s.length();i++){
-                map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+
+        int[] freq = new int[26];
+
+        for(String word : words){
+            for(char c : word.toCharArray()){
+                freq[c - 'a']++;
             }
         }
 
-        for(Map.Entry<Character,Integer>m:map.entrySet()){
-            if(m.getValue()%words.length!=0)return false;
+        for(int count : freq){
+            if(count % words.length != 0){
+                return false;
+            }
         }
+
         return true;
     }
 }
