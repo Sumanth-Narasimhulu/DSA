1ae6eb877c060faf68ca89a6e59bc4bbdc88d466
Sumanth-Narasimhulu <137056321+Sumanth-Narasimhulu@users.noreply.github.com>
2026-07-21 21:42:29 +0530

    37 ms (18.05%) 46.7 MB (21.46%) 07-21-2026 0567-permutation-in-string Medium .java

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        return findAnagrams(s2,s1);
+    }
+    public boolean findAnagrams(String s, String p) {
+        
+        HashMap<Character,Integer>map = new HashMap<>();
+        char[]pc = p.toCharArray();
+        for(int i=0;i<pc.length;i++){
+            map.put(pc[i],map.getOrDefault(pc[i],0)+1);
+        }
+        int n = p.length();
+        //List<Integer>res = new ArrayList<>();   
+
+        int i=0;
+        char[]nums = s.toCharArray();
+        for(int j=0;j<s.length();j++){
+            char c = nums[j];
+            if(map.containsKey(c)){
+              int freq = map.get(c);
+              map.put(c,freq-1);
+            }
+            if(j-i+1<p.length()){
+                
+                continue;
+            }
+            if(j-i+1 == p.length()){
+                boolean flag = checkEmpty(map);
+                if(flag){
+                    return true;
+                }
+                char c1 = nums[i];
+                if(map.containsKey(c1)){
+                    int freq = map.get(c1);
+                    map.put(c1,freq+1);
+                }
+                i++;
+            }
+        }
+        return false;
+
+    }
+    public boolean checkEmpty(HashMap<Character,Integer>map){
+        for(int ele:map.values()){
+            if(ele!=0)return false;
+        }
+        return true;
+    }
+}
